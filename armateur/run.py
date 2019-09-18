2f26a84ced0f1e8ccfc4f02e3b1b8f0b7631b927
baldychristophe <baldychristophe@gmail.com>
2019-09-18 12:10:40 +0200

    run: with scrolling

--- armateur/run.py
+++ armateur/run.py
@@ -19,26 +19,43 @@ def read_map():
     return map
 
 
-def main():
-    white = (255, 255, 255)
+def draw_map(map, cursor_offset):
     blue = (0, 0, 255)
     green = (0, 255, 0)
+
+    for i, line in enumerate(map):
+        if i < cursor_offset[1]:
+            pass
+        if ((i * pixel_size) - cursor_offset[1]) > screen_width:
+            print('break i: %s' % i)
+            break
+        for j, col in enumerate(line):
+            if j < cursor_offset[0]:
+                pass
+            if ((j * pixel_size) - cursor_offset[0]) > screen_height:
+                print('break j: %s' % j)
+                break
+            if int(col) < 0:
+                color = blue
+            else:
+                color = green
+            pygame.draw.rect(screen, color, (int((j * pixel_size) - cursor_offset[1]), int((i * pixel_size) - cursor_offset[0]), pixel_size, pixel_size))
+
+    pygame.display.flip()
+
+
+def main():
+    white = (255, 255, 255)
+
     DOWN_KEY = 274
+
+    cursor_offset = (0, 0)
 
     pygame.init()
     pygame.display.set_caption("Armateur")
 
     map = read_map()
-
-    for i, line in enumerate(map):
-        for j, col in enumerate(line):
-            if int(col) < 0:
-                color = blue
-            else:
-                color = green
-            pygame.draw.rect(screen, color, (j * pixel_size, i * pixel_size, pixel_size, pixel_size))
-
-    pygame.display.flip()
+    draw_map(map, cursor_offset)
 
     running = True
     fps_limit = 60
@@ -50,7 +67,9 @@ def main():
                 running = False
 
             if event.type == pygame.KEYUP and event.key == DOWN_KEY:
-                pass
+                print('key up event')
+                cursor_offset = (cursor_offset[0] + 50, cursor_offset[1])
+                draw_map(map, cursor_offset)
 
 
 if __name__ == '__main__':
