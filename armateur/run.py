d6ece7f9d4a3052359a5840efde56bf852859ac8
baldychristophe <baldychristophe@gmail.com>
2019-09-18 18:48:59 +0200

    run: scrolling activated

--- armateur/run.py
+++ armateur/run.py
@@ -3,14 +3,14 @@ import random
 import pygame
 
 
-screen_size = screen_width, screen_height = 1441, 1441
+screen_size = screen_width, screen_height = 1080, 1080
 screen = pygame.display.set_mode(screen_size)
 
-pixel_size = 2
+pixel_size = 10
 
 
 def read_map():
-    f = open('France_250_ASC_L93.OCEAN0.XL.fdf')
+    f = open('France_250_ASC_L93.OCEAN0.S.fdf')
     map = []
     for line in f:
         cols = line.split(' ')
@@ -22,33 +22,23 @@ def read_map():
 def draw_map(map, cursor_offset):
     blue = (0, 0, 255)
     green = (0, 255, 0)
+    white = (255, 255, 255)
 
-    for i, line in enumerate(map):
-        if i < cursor_offset[1]:
-            pass
-        if ((i * pixel_size) - cursor_offset[1]) > screen_width:
-            print('break i: %s' % i)
-            break
-        for j, col in enumerate(line):
-            if j < cursor_offset[0]:
-                pass
-            if ((j * pixel_size) - cursor_offset[0]) > screen_height:
-                print('break j: %s' % j)
-                break
-            if int(col) < 0:
+    screen.lock()
+    screen.fill(white)
+    for i in range(cursor_offset[0], int((screen_height / pixel_size) + cursor_offset[0])):
+        for j in range(cursor_offset[1], int((screen_width / pixel_size) + cursor_offset[1])):
+            if int(map[i][j]) < 0:
                 color = blue
             else:
                 color = green
-            pygame.draw.rect(screen, color, (int((j * pixel_size) - cursor_offset[1]), int((i * pixel_size) - cursor_offset[0]), pixel_size, pixel_size))
+            pygame.draw.rect(screen, color, (int((j - cursor_offset[1]) * pixel_size), int((i - cursor_offset[0]) * pixel_size), pixel_size, pixel_size))
 
+    screen.unlock()
     pygame.display.flip()
 
 
 def main():
-    white = (255, 255, 255)
-
-    DOWN_KEY = 274
-
     cursor_offset = (0, 0)
 
     pygame.init()
@@ -58,17 +48,28 @@ def main():
     draw_map(map, cursor_offset)
 
     running = True
-    fps_limit = 60
+    fps_limit = 20
     clock = pygame.time.Clock()
     while running:
         clock.tick(fps_limit)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.key == pygame.K_ESCAPE:
                 running = False
 
-            if event.type == pygame.KEYUP and event.key == DOWN_KEY:
-                print('key up event')
+            if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
                 cursor_offset = (cursor_offset[0] + 50, cursor_offset[1])
+                draw_map(map, cursor_offset)
+
+            if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+                cursor_offset = (cursor_offset[0] - 50, cursor_offset[1])
+                draw_map(map, cursor_offset)
+
+            if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
+                cursor_offset = (cursor_offset[0], cursor_offset[1] + 50)
+                draw_map(map, cursor_offset)
+
+            if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
+                cursor_offset = (cursor_offset[0], cursor_offset[1] - 50)
                 draw_map(map, cursor_offset)
 
 
