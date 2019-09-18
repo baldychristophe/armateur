1e330c057f073d02593129f41247e8596ab1cf0b
baldychristophe <baldychristophe@gmail.com>
2019-09-18 11:42:39 +0200

    run: use a predefined map

--- armateur/run.py
+++ armateur/run.py
@@ -3,46 +3,54 @@ import random
 import pygame
 
 
-screen_size = screen_width, screen_height = 600, 450
+screen_size = screen_width, screen_height = 1441, 1441
 screen = pygame.display.set_mode(screen_size)
 
+pixel_size = 2
 
-class Pixel:
-    def __init__(self, color, position):
-        self.color = color
-        self.position = position
 
-    def display(self):
-        pygame.draw.rect(screen, self.color, ((*self.position), 50, 50))
+def read_map():
+    f = open('France_250_ASC_L93.OCEAN0.XL.fdf')
+    map = []
+    for line in f:
+        cols = line.split(' ')
+        map.append(cols)
+
+    return map
+
 
 def main():
     white = (255, 255, 255)
     blue = (0, 0, 255)
     green = (0, 255, 0)
+    DOWN_KEY = 274
 
     pygame.init()
-
     pygame.display.set_caption("Armateur")
-    screen.fill(white)
-    pixels = []
-    for i in range(int(screen_width / 50)):
-        for j in range(int(screen_height / 50)):
-            color = blue if (i + j) % 2 == 0 else green
-            pixels.append(Pixel(color, (i * 50, j * 50)))
 
-    for pixel in pixels:
-        pixel.display()
+    map = read_map()
+
+    for i, line in enumerate(map):
+        for j, col in enumerate(line):
+            if int(col) < 0:
+                color = blue
+            else:
+                color = green
+            pygame.draw.rect(screen, color, (j * pixel_size, i * pixel_size, pixel_size, pixel_size))
 
     pygame.display.flip()
 
     running = True
+    fps_limit = 60
+    clock = pygame.time.Clock()
     while running:
-        # event handling, gets all event from the event queue
+        clock.tick(fps_limit)
         for event in pygame.event.get():
-            # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
                 running = False
+
+            if event.type == pygame.KEYUP and event.key == DOWN_KEY:
+                pass
 
 
 if __name__ == '__main__':
