4e9bc31bfd720b5d0ef1f41a9254cd774741e90c
baldychristophe <baldychristophe@gmail.com>
2019-09-19 12:23:55 +0200

    run: fill map with hexagons

--- armateur/run.py
+++ armateur/run.py
@@ -1,4 +1,4 @@
-import random
+import math
 
 import pygame
 
@@ -6,11 +6,28 @@ import pygame
 screen_size = screen_width, screen_height = 1080, 1080
 screen = pygame.display.set_mode(screen_size)
 
-pixel_size = 10
+pixel_size = 20
+
+blue = (0, 0, 255)
+green = (0, 255, 0)
+white = (255, 255, 255)
 
 
-class Octagon(pygame.sprite.Sprite):
-    pass
+class Hexagon(pygame.sprite.Sprite):
+    def __init__(self, center, radius, color=green):
+        super().__init__()
+        self.center = center
+        self.radius = radius
+        self.color = color
+        self.drawing_vector = pygame.math.Vector2(0, self.radius)
+
+    def display(self):
+        points = []
+        for i in range(6):
+            point = (self.center + self.drawing_vector.rotate(i * 60))
+            points.append(tuple(map(int, point)))
+
+        pygame.draw.polygon(screen, self.color, points, 1)
 
 
 
@@ -24,35 +41,48 @@ def read_map():
     return map
 
 
-def draw_map(map, cursor_offset):
-    blue = (0, 0, 255)
-    green = (0, 255, 0)
-    white = (255, 255, 255)
-
+def draw_map(game_map, scroll_offset):
     screen.lock()
     screen.fill(white)
-    for i in range(cursor_offset[0], int((screen_height / pixel_size) + cursor_offset[0])):
-        for j in range(cursor_offset[1], int((screen_width / pixel_size) + cursor_offset[1])):
-            if int(map[i][j]) < 0:
-                color = blue
-            else:
-                color = green
-            inside = pygame.draw.rect(screen, color, (int((j - cursor_offset[1]) * pixel_size), int((i - cursor_offset[0]) * pixel_size), pixel_size, pixel_size))
-            outside = pygame.draw.rect(screen, (0, 0, 0), inside, 1)
+    # for i in range(scroll_offset[0], int((screen_height / pixel_size) + scroll_offset[0])):
+    #     for j in range(scroll_offset[1], int((screen_width / pixel_size) + scroll_offset[1])):
+    #         if int(map[i][j]) < 0:
+    #             color = blue
+    #         else:
+    #             color = green
+    #         inside = pygame.draw.rect(screen, color, (int((j - scroll_offset[1]) * pixel_size), int((i - scroll_offset[0]) * pixel_size), pixel_size, pixel_size))
+    #         outside = pygame.draw.rect(screen, (0, 0, 0), inside, 1)
+
+    radius = 36
+
+    hexs = []
+    for i in range(int(screen_width / (radius * math.sin(math.radians(30)) * 2))):
+        for j in range(int(screen_height / (radius * 2 * math.cos(math.radians(30))))):
+            hexs.append(
+                Hexagon(
+                    (
+                        (j * 2 * math.cos(math.radians(30)) * radius) + (((i + 1) % 2) * math.cos(math.radians(30)) * radius),
+                        (i * (radius + (math.sin(math.radians(30)) * radius))) + radius,
+                    ),
+                    radius,
+                )
+            )
+
+    for hex in hexs:
+        hex.display()
 
     screen.unlock()
     pygame.display.flip()
-    print('end draw')
 
 
 def main():
-    cursor_offset = (0, 0)
+    scroll_offset = (0, 0)
 
     pygame.init()
     pygame.display.set_caption("Armateur")
 
     map = read_map()
-    draw_map(map, cursor_offset)
+    draw_map(map, scroll_offset)
 
     running = True
     fps_limit = 20
@@ -64,20 +94,20 @@ def main():
                 running = False
 
             if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
-                cursor_offset = (cursor_offset[0] + 1, cursor_offset[1])
-                draw_map(map, cursor_offset)
+                scroll_offset = (scroll_offset[0] - 1, scroll_offset[1])
+                draw_map(map, scroll_offset)
 
             if event.type == pygame.KEYUP and event.key == pygame.K_UP:
-                cursor_offset = (cursor_offset[0] - 1, cursor_offset[1])
-                draw_map(map, cursor_offset)
+                scroll_offset = (scroll_offset[0] + 1, scroll_offset[1])
+                draw_map(map, scroll_offset)
 
             if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
-                cursor_offset = (cursor_offset[0], cursor_offset[1] + 1)
-                draw_map(map, cursor_offset)
+                scroll_offset = (scroll_offset[0], scroll_offset[1] - 1)
+                draw_map(map, scroll_offset)
 
             if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
-                cursor_offset = (cursor_offset[0], cursor_offset[1] - 1)
-                draw_map(map, cursor_offset)
+                scroll_offset = (scroll_offset[0], scroll_offset[1] + 1)
+                draw_map(map, scroll_offset)
 
 
 if __name__ == '__main__':
